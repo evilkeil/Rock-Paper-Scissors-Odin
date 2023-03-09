1bcaf425916f3d2cad00b3d9f4b8953af97a03ed
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-03-10 00:52:42 +0900

    Add comments and fix typo

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,8 +1,9 @@
-const select= prompt("Rock,Paper,Scisors","choose");
+const select= prompt("Rock,Paper,Scissors","choose");   //Makes the user choose Rock,Paper or Scissors
 
 console.log(select);
 
-let choice = (select) => {
+let choice = (select) => { 
+ //take the players choice and convert it to lowercase so that itll be case insensitive
     select = select.toLowerCase();
    return select;
 
@@ -10,6 +11,7 @@ let choice = (select) => {
 
 console.log(choice(select));
 
-const playerSelection= choice(select);
+
+const playerSelection= choice(select);  //use the choice which is converted to lowercase and assign to playerSelection which is the first parameter.
 
 console.log(playerSelection);
