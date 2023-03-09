4012055d767c1425d3e40f3b98586ea2ea9c12fa
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-03-10 01:06:01 +0900

    create a function that generates a random number from 0,1,2 and assign it to a constant

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -15,3 +15,18 @@ console.log(choice(select));
 const playerSelection= choice(select);  //use the choice which is converted to lowercase and assign to playerSelection which is the first parameter.
 
 console.log(playerSelection);
+
+
+ function computerSelectionRand(){
+
+    //create a function that generates 3 numbers : 0,1 and 2.
+    return Math.floor(Math.random() * 3);
+
+ }
+
+ 
+ const PCChoice = computerSelectionRand();
+ console.log(PCChoice);
+
+ 
+ 
