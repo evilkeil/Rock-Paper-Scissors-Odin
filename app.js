d23e309bee21803fbd9ab703db2b2efafef147ca
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-03-20 22:38:16 +0900

    make it so that the result will be displayed by an image

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let computerVariable;
 const choices = document.querySelector('#choices ul');
 
 choices.addEventListener('click',function(e){
-    if(e.target.className === "rock"){
+    if(e.target.className === "rock"){ //asign rock paper or scissors to the img
         playerVariable = 0 ;
     }else if(e.target.className === "paper"){
         playerVariable = 1;
@@ -14,13 +14,41 @@ choices.addEventListener('click',function(e){
         playerVariable = 2;
     }else{}
 
-    computerVariable = computerSelectionRand();
+    computerVariable = computerSelectionRand(); //fining the slection of PC
 
     let finalResult = RPC(playerVariable,computerVariable);
     
    console.log(finalResult);
-   console.log(playerVariable);
-   console.log(computerVariable);
+    let victory = "win";
+    let defeat = "lose";
+    let draw = "draw";
+
+    const winReaction = document.querySelector('.resultImage.win');
+    const loseReaction = document.querySelector('.resultImage.lose');
+    const drawReaction =document.querySelector('.resultImage.draw');
+
+    // console.log(winReaction);
+    // console.log(loseReaction);
+    // console.log(drawReaction);
+
+    if(finalResult.includes(victory)){
+       winReaction.classList.add('active');
+       loseReaction.classList.remove('active');
+       drawReaction.classList.remove('active');
+    }else if(finalResult.includes(defeat)){
+        loseReaction.classList.add('active');
+        winReaction.classList.remove('active');
+        drawReaction.classList.remove('active');
+    }else if(finalResult.includes(draw)){
+        drawReaction.classList.add('active');
+        winReaction.classList.remove('active');
+        loseReaction.classList.remove('active');
+    }
+
+   
+
+   
+   
 
     
 
@@ -40,27 +68,35 @@ function computerSelectionRand(){
     let player = playerSelection; //assign temp variables to make the code easier to read.
     let comp = computerSelection;
     let result;
+    let resultfinal;
     switch (true){
         case (player === comp):
             result = `you both picked ${player} so its a draw`;
+            resultfinal = "draw";
             break;
         case (player === 0 && comp === 1):
             result ="your opponent chose papers.You lose because `paper covers rock`. ";
+            resultfinal ="lose";
             break;
         case (player === 0 && comp === 2):
             result="your opponent chose scissors.You win because `rock beats scissors`.";
+            resultfinal ="win";
             break;
         case (player === 1 && comp ===0):
             result="your opponent chose rock.You win because `paper covers rock`.";
+            resultfinal ="win";
             break;
         case (player === 1 && comp ===2):
             result="your opponent chose scissors.You lose because `scissors cuts paper `.";
+            resultfinal ="lose";
             break;
         case (player === 2 && comp ===0):
             result="your opponent chose rock,You lose because `rock beats scissors`";
+            resultfinal ="lose";
             break;
         case (player === 2 && comp ===1):
             result="your opponent chose paper,You win because `scissors cuts paper`";
+            resultfinal ="win";
             break;
         default:
             result="please choose a correct choice";
@@ -70,3 +106,13 @@ function computerSelectionRand(){
     return result;
 }
 
+// function checkIfContains(gameResult){
+//     let victory = "win";
+//     let defeat = "lose";
+//     let draw = "draw";
+
+//     if(gameResult.includes(victory)){
+//         return gameResult;
+//     }else if
+
+// }
